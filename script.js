c9d13c06c5571f6cd3b059295e72fe9a929051b4
hj-prk <hpark@u.nus.edu>
2023-07-03 13:58:18 +0900

    Move json file data to json object

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const surnameloc = "/json files/surname.json";
-const f_firstloc = "/json files/f_first.json";
-const f_secondloc = "/json files/f_second.json";
+const surname = '{"kor":"김", "eng":"Kim", "jap":"キム", "schn":"金","tchn":"金"}\n'
++'{"kor":"이", "eng":"Lee", "jap":"イ", "schn":"李", "tchn":"李"}';
 
-const ran1 = "0";
-const ran2 = "0";
-const ran3 = "0";
+const f_first = '{"kor":"수", "eng":"Soo", "jap":"ス", "schn":"", "tchn":"秀", "mean":"Outstanding"}\n'
++'{"kor":"현", "eng":"Hyun", "jap":"ヒョン", "schn":"贤", "tchn":"賢", "mean":"Clever"}';
+
+const f_second = '{"kor":"진", "eng":"Jin", "jap":"ジン", "schn":"真", "tchn":"眞", "mean":"honest"}\n'
++'{"kor":"정", "eng":"Jeong",  "jap":"ジョン", "schn":"淨", "tchn":"净", "mean":"clean"}';
+
 
 let genderval;
 
@@ -21,4 +23,11 @@ form.addEventListener("submit", (e) => {
 
 //console.log(genderval); //test
 
-import data from ''
+$(document).ready(function(){
+    $.getJSON("letters.json", function(data){
+        console.log(data.surname);
+        console.log(data); 
+    }).fail(function(){
+        console.log("An error has occurred.");      
+    });
+});
